3a5c06478de9818e7304ed6d2949bbe50769a36a
kazuki827 <xm.kazuki827@gmail.com>
2021-12-31 15:04:40 +0900

    if

---
+++ if.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func by2(num int) string {
+	if num%2 == 0 {
+		return "ok"
+	} else {
+		return "no"
+	}
+}
+
+func main() {
+	result := by2(10)
+	if result == "ok" {
+		fmt.Println("great")
+	}
+
+	// 簡略化
+	if result2 := by2(10); result2 == "ok" {
+		fmt.Println("great2")
+	}
+	// fmt.Println(result2) NG if分の中のみ
+
+	num := 9
+	if num%2 == 0 {
+		fmt.Println("by 2")
+	} else if num%3 == 0 {
+		fmt.Println("by 3")
+	} else {
+		fmt.Println("else")
+	}
+
+	x, y := 10, 10
+	if x == 10 && y == 10 {
+		fmt.Println("&&")
+	}
+
+	if x == 10 || y == 10 {
+		fmt.Println("||")
+	}
+}
