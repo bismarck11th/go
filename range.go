c3ec1a50300e97722deb8f064a7be16b57c2b806
kazuki827 <xm.kazuki827@gmail.com>
2022-01-01 09:20:05 +0900

    slice, map　exe

--- range.go
+++ range.go
@@ -34,4 +34,41 @@ func main() {
 	for _, v := range m {
 		fmt.Println(v)
 	}
+
+	// sliceから最小値取得
+	slice := []int{100, 300, 23, 11, 2, 4, 6, 4}
+
+	var min int
+	for i := 0; i < len(slice); i++ {
+
+		for i, num := range slice {
+			if i == 0 {
+				min = num
+				continue
+			}
+
+			if min >= num {
+				min = num
+			}
+		}
+	}
+
+	fmt.Println("最小値:", min)
+
+	// mapのvalueの合計値取得
+	m2 := map[string]int{
+		"apple":  200,
+		"banana": 300,
+		"grapes": 150,
+		"orange": 80,
+		"papaya": 500,
+		"kiwi":   90,
+	}
+
+	sum := 0
+	for _, v := range m2 {
+		sum += v
+	}
+
+	fmt.Println(sum)
 }
